3be35c2db1ccfc58b0dfba11197878cbc729b635
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2021-07-28 21:05:11 +0530

    Initial commit

---
+++ app/src/main/java/com/xyz/childrenbank/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.xyz.childrenbank
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.xyz.childrenbank.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.card.setOnClickListener {
+            Toast.makeText(this, "You have clicked upon a card!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
